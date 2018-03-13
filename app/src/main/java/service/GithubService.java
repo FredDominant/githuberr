667a855eb69bf827d82e08569beb9913d2a586bc
Fred <fredadewole@gmail.com>
2018-03-13 09:33:31 +0100

    feature(Retrofit): create retrofit instance

---
+++ app/src/main/java/service/GithubService.java
@@ -0,0 +1,26 @@
+package service;
+
+import model.GithubUsers;
+import model.GithubUsersResponse;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Fred Adewole on 12/03/2018.
+ */
+
+public class GithubService {
+    private Retrofit retrofit = null;
+
+    public GithubUsersAPI getApi() {
+        String BASE_URl = "https://api.github.com";
+        if (retrofit == null) {
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(GithubUsersAPI.class);
+    }
+}
