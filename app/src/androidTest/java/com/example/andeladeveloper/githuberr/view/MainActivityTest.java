0f331121b58f7d2cbebc835a1c30652113bc2318
Fred <fredadewole@gmail.com>
2018-03-27 19:21:43 +0100

    chore(MainActivity Test): add test suite for MainActivity

---
+++ app/src/androidTest/java/com/example/andeladeveloper/githuberr/view/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.andeladeveloper.githuberr.view;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import com.example.andeladeveloper.githuberr.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch() {
+        View recyclerView = mainActivity.findViewById(R.id.recyclerView);
+        assertNotNull(recyclerView);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+
+}
