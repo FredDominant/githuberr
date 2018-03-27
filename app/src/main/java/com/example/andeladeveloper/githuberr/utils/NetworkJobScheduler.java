8fc09b8a98f46e4c83bd256826e51229d5ee6971
Fred <fredadewole@gmail.com>
2018-03-27 18:47:20 +0100

    merge to local develop

--- app/src/main/java/com/example/andeladeveloper/githuberr/utils/NetworkJobScheduler.java
+++ app/src/main/java/com/example/andeladeveloper/githuberr/utils/NetworkJobScheduler.java
@@ -11,14 +11,6 @@ public class NetworkJobScheduler extends JobService {
     public boolean onStartJob(JobParameters params) {
         Log.i("", "onStartJob in JobService Was called");
         Toast.makeText(getApplicationContext(), "Job started", Toast.LENGTH_LONG).show();
-
-//        NetworkUtility networkUtility = new NetworkUtility(getApplicationContext());
-//
-//        if (!networkUtility.isConnected()) {
-//            Toast.makeText(getApplicationContext(), "Disconnected Do other stuff here...",
-//              Toast.LENGTH_LONG).show();
-//            jobFinished(params, true);
-//        }
         return false;
     }
 
