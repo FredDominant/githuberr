07771bee7a0957e7efddabdcc3136ad62400ce9a
Fred Adewole <fredadewole@gmail.com>
2018-06-18 12:22:43 +0100

    Ch/156185242/tests (#15)
    * merge to local develop
    
    * chore(MainActivity Test): add test suite for MainActivity
    
    * chore(MainActivity Test): add more test suites to check view components
    
    * chore(
    MainActivity Test): implement espresso - WIP
    
    * chore(
    MainActivity Test): implement espresso - WIP
    
    * chore(Unit tests): add more unit test suites for Utils folder
    
    * feature(Login activity): add login activity

---
+++ app/src/main/java/com/example/andeladeveloper/githuberr/view/WelcomeActivity.java
@@ -0,0 +1,69 @@
+package com.example.andeladeveloper.githuberr.view;
+
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.andeladeveloper.githuberr.R;
+
+/**
+ * The type Welcome activity.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    /**
+     * The Email.
+     */
+    EditText email, /**
+     * The Password.
+     */
+    password;
+    /**
+     * The Login button.
+     */
+    Button loginButton;
+    /**
+     * The Container.
+     */
+    ConstraintLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        container = findViewById(R.id.layout_welcome_container);
+        email = findViewById(R.id.text_email);
+        password = findViewById(R.id.text_password);
+        loginButton = findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserIn();
+            }
+        });
+    }
+
+    /**
+     * Log user in.
+     */
+
+    private void loginUserIn() {
+        String emailValue = email.getText().toString();
+        String passwordValue = password.getText().toString();
+
+        if (emailValue.isEmpty() || passwordValue.isEmpty()) {
+            Snackbar.make(container, "One or more fields are empty!", Snackbar.LENGTH_LONG)
+                    .show();
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+
+    }
+}
